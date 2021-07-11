716715ff18b5ed40b1a9d5545f16ddc0550dbf0f
Jared Stemper <jaredastemper@gmail.com>
2021-07-11 12:45:15 -0500

    added monitor brightness scripts

---
+++ Misc/externalMonitorBrightness.sh
@@ -0,0 +1,13 @@
+#grab external monitor's brightness using xrandr
+BRIGHTNESS=`xrandr --verbose | grep -m 2 -i brightness | tail -n 1 | cut -f2 -d ' '`
+
+#update brightness by given input
+BRIGHTNESS=`awk '{print $1+$2}' <<<"${BRIGHTNESS} $1"`
+
+if [ $BRIGHTNESS = "0" ]; then
+	xrandr --output HDMI-1 --brightness 0;
+elif [ $BRIGHTNESS = "1" ]; then
+	xrandr --output HDMI-1 --brightness 1;
+else
+	xrandr --output HDMI-1 --brightness $BRIGHTNESS;
+fi
